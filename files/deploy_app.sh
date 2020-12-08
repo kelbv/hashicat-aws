f349196bb5d3cf76222b5b0912fec185da4eb607
kelbv <kelvin.tooth@gmail.com>
2020-12-08 17:00:49 +0000

    adding word definitely

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. THis is not a picture of Barney.
+  Welcome to ${PREFIX}'s app. THis is definitely not a picture of Barney.
   <!-- END -->
 
   </div>
